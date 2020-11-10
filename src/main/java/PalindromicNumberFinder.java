04023ccbe910fc8c44b8b05e038d56af34fc76b0
KaMeron Hopkins <620508@aacps.org>
2020-11-10 18:29:04 +0000

    FFS

--- src/main/java/PalindromicNumberFinder.java
+++ src/main/java/PalindromicNumberFinder.java
@@ -17,6 +17,7 @@ public class PalindromicNumberFinder {
       for(int i = num + 1; i > num; i++){
         if(testPalindromicNum(i) == true){
           NP = i;
+          break;
         }else if(testPalindromicNum(i) == false){
           NP = 1;
         }
